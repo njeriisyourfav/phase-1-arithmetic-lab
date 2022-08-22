8178446a53a147ceebfa8e53215bbd4947618ccc
lilian njeri ndungu <lilian.ndungu@student.moringaschool.com>
2022-08-22 15:00:56 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+const num1= 31;
+const num2= 2;
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 20
+let num4 = 5
+let mod = num3 / num4;
+
+let a = 4
+let b = 8
+let c = 16
+let d = 20
+let max = Math.max(a, b, c, d)
